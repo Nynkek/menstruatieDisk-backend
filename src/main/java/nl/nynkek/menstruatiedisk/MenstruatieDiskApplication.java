bd2716c0fd0fbeea0cc6913b95a9e508d10e4e87
Nynke <35605149+Nynkek@users.noreply.github.com>
2022-06-10 14:52:47 +0200

    first commit

---
+++ src/main/java/nl/nynkek/menstruatiedisk/MenstruatieDiskApplication.java
@@ -0,0 +1,13 @@
+package nl.nynkek.menstruatiedisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenstruatieDiskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenstruatieDiskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/nynkek/menstruatiedisk/MenstruatieDiskApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.nynkek.menstruatiedisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenstruatieDiskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
