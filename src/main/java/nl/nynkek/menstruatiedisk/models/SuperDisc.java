c134c66359d665b948057cd29f15043f7e641538
Nynke <35605149+Nynkek@users.noreply.github.com>
2022-06-20 16:05:40 +0200

    datbase gevuld met dummy data

--- src/main/java/nl/nynkek/menstruatiedisk/models/SuperDisc.java
+++ src/main/java/nl/nynkek/menstruatiedisk/models/SuperDisc.java
@@ -25,4 +25,123 @@ abstract class SuperDisc {
     @Enumerated(EnumType.ORDINAL)
     private Material material;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getRimWidth() {
+        return rimWidth;
+    }
+
+    public void setRimWidth(int rimWidth) {
+        this.rimWidth = rimWidth;
+    }
+
+    public boolean isReusable() {
+        return isReusable;
+    }
+
+    public void setReusable(boolean reusable) {
+        isReusable = reusable;
+    }
+
+    public String getDesignFeature() {
+        return designFeature;
+    }
+
+    public void setDesignFeature(String designFeature) {
+        this.designFeature = designFeature;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+
+    public String getFirmness() {
+        return firmness;
+    }
+
+    public void setFirmness(String firmness) {
+        this.firmness = firmness;
+    }
+
+    public String getLinkToStore() {
+        return linkToStore;
+    }
+
+    public void setLinkToStore(String linkToStore) {
+        this.linkToStore = linkToStore;
+    }
+
+    public String getLinkToReview() {
+        return linkToReview;
+    }
+
+    public void setLinkToReview(String linkToReview) {
+        this.linkToReview = linkToReview;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public boolean isAvailableInNL() {
+        return isAvailableInNL;
+    }
+
+    public void setAvailableInNL(boolean availableInNL) {
+        isAvailableInNL = availableInNL;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,83 @@
--- INSERT INTO discs (id, brand, capacity, design_feature, is_reusable, model, name, rim_width, width, material, shape, firmness, is_available_in_nl, link_to_store, link_to_review, image) VALUES (1, 'Intimina', 20, 'geen steel', true, 'OS', 'Ziggy', 40, 40, 1, 'a','b', true,'c','d','e');
+INSERT INTO discs
+(id, brand, capacity, design_feature, is_reusable, model, name, rim_width, width, material, shape, firmness,
+ is_available_in_nl, link_to_store, link_to_review, image)
+VALUES (1, 'Flex', 30, 'geen steel', false, 'OS', 'Flex disc', 8, 70, 2, 'rond', 'hard', false, 'https://flexfits.com/',
+        '', 'https://cupkiezer.nl/wp-content/uploads/2017/08/yuuki_cupbox-copy.jpg');
 
-INSERT INTO users (username, email_adress, password, role) VALUES ('Nynke','info@email.nl','geheim0','ADMIN');
-INSERT INTO users (username, email_adress, password, role) VALUES ('User','info@email.nl','geheim1','gebruiker');
-INSERT INTO users (username, email_adress, password, role) VALUES ('Pim','info@email.nl','geheim2','diskToevoeger');
+INSERT INTO discs
+(id, brand, capacity, design_feature, is_reusable, model, name, rim_width, width, material, shape, firmness,
+ is_available_in_nl, link_to_store, link_to_review, image)
+VALUES (2, 'Lumma', 30, 'Heeft een siliconen touwtje', true, 'S', 'Lumma Unique', 6, 53, 1, 'rond', 'zacht', false,
+        'https://lummacups.com/?aff=28',
+        'https://cupkiezer.nl/2020/08/review-lumma-menstruatiedisc-ervaring-van-katharina/',
+        'https://cupkiezer.nl/wp-content/uploads/2017/08/yuuki_cupbox-copy.jpg');
 
--- INSERT INTO brands (id, name, website, description) VALUES (1,'pimnynkeprint','https://website.com','een leuke webshop goed merk');
--- INSERT INTO brands (id, name, website, description) VALUES (2,'Ziggy','https://ziggy.com','Ziggy heeft veel discs');
--- INSERT INTO brands (id, name, website, description) VALUES (3,'meluna','https://meluna.com','meluna is een merk cup');
+INSERT INTO discs
+(id, brand, capacity, design_feature, is_reusable, model, name, rim_width, width, material, shape, firmness,
+ is_available_in_nl, link_to_store, link_to_review, image)
+VALUES (3, 'Lumma', 55, 'Heeft een siliconen touwtje', true, 'M', 'Lumma Unique', 6, 63, 1, 'rond', 'zacht', false,
+        'https://lummacups.com/?aff=28',
+        'https://cupkiezer.nl/2020/08/review-lumma-menstruatiedisc-ervaring-van-katharina/',
+        'https://cupkiezer.nl/wp-content/uploads/2017/08/yuuki_cupbox-copy.jpg');
 
--- INSERT INTO pending_discs (id, created_date, username) VALUES ('1','10-10-1988','Nynke');
+INSERT INTO discs
+(id, brand, capacity, design_feature, is_reusable, model, name, rim_width, width, material, shape, firmness,
+ is_available_in_nl, link_to_store, link_to_review, image)
+VALUES (4, 'Lumma', 60, 'Heeft een siliconen touwtje', true, 'L', 'Lumma Unique', 6, 68, 1, 'rond', 'zacht', false,
+        'https://lummacups.com/?aff=28',
+        'https://cupkiezer.nl/2020/08/review-lumma-menstruatiedisc-ervaring-van-katharina/',
+        'https://cupkiezer.nl/wp-content/uploads/2017/08/yuuki_cupbox-copy.jpg');
+
+INSERT INTO discs
+(id, brand, capacity, design_feature, is_reusable, model, name, rim_width, width, material, shape, firmness,
+ is_available_in_nl, link_to_store, link_to_review, image)
+VALUES (5, 'Nixit', 45, 'erg zacht', true, 'OS', 'Nixit cup', 10, 70, 1, 'rond', 'zacht', false,
+        'http://i.refs.cc/IqWu8nVq?smile_ref=eyJzbWlsZV9zb3VyY2UiOiJzbWlsZV91aSIsInNtaWxlX21lZGl1bSI6IiIsInNtaWxlX2NhbXBhaWduIjoicmVmZXJyYWxfcHJvZ3JhbSIsInNtaWxlX2N1c3RvbWVyX2lkIjo0NDI3MTM4Mjd9',
+        '', 'https://cupkiezer.nl/wp-content/uploads/2017/08/yuuki_cupbox-copy.jpg');
+
+INSERT INTO discs
+(id, brand, capacity, design_feature, is_reusable, model, name, rim_width, width, material, shape, firmness,
+ is_available_in_nl, link_to_store, link_to_review, image)
+VALUES (6, 'Ziggy', 76, 'doordat de disk niet rond is kan je hem maar op een manier indoen.', true, 'OS', 'Ziggy disc',
+        7, 76, 1, 'ovaal', 'zacht', true,
+        'https://partner.bol.com/click/click?p=2&t=url&s=49589&f=TXL&url=https%3A%2F%2Fwww.bol.com%2Fnl%2Fp%2Fintimina-ziggy-cup%2F9200000130869965%2F&name=Intimina%20-%20Ziggy%20Cup&subid=blog',
+        '', 'https://cupkiezer.nl/wp-content/uploads/2017/08/yuuki_cupbox-copy.jpg');
+
+INSERT INTO discs
+(id, brand, capacity, design_feature, is_reusable, model, name, rim_width, width, material, shape, firmness,
+ is_available_in_nl, link_to_store, link_to_review, image)
+VALUES (7, 'Bamboozy', 50, 'Nederlands merk', true, 'M', 'Bamboozy disk', 7, 65, 1, 'rond', 'medium', true, '',
+        'https://cupkiezer.nl/2022/04/nederlandse-disk-bamboozy-verkoopt-eigen-menstruatiedisks/',
+        'https://cupkiezer.nl/wp-content/uploads/2017/08/yuuki_cupbox-copy.jpg');
+
+INSERT INTO discs
+(id, brand, capacity, design_feature, is_reusable, model, name, rim_width, width, material, shape, firmness,
+ is_available_in_nl, link_to_store, link_to_review, image)
+VALUES (8, 'Bamboozy', 65, 'Nederlands merk', true, 'L', 'Bamboozy disk', 7, 70, 1, 'rond', 'medium', true, '',
+        'https://cupkiezer.nl/2022/04/nederlandse-disk-bamboozy-verkoopt-eigen-menstruatiedisks/',
+        'https://cupkiezer.nl/wp-content/uploads/2017/08/yuuki_cupbox-copy.jpg');
+
+
+INSERT INTO users (username, email_adress, password, role)
+VALUES ('Nynke', 'info@email.nl', 'geheim0', 'ADMIN');
+INSERT INTO users (username, email_adress, password, role)
+VALUES ('User', 'info@email.nl', 'geheim1', 'gebruiker');
+INSERT INTO users (username, email_adress, password, role)
+VALUES ('Pim', 'info@email.nl', 'geheim2', 'diskToevoeger');
+
+
+INSERT INTO brands (id, name, website, description)
+VALUES (1, 'Bamboozy', 'https://website.com', 'Bamboozy heeft twee disks: een M (65mm) en een grotere L (70mm).');
+INSERT INTO brands (id, name, website, description)
+VALUES (2, 'Ziggy', 'https://website.com', 'Ziggy heeft veel discs');
+INSERT INTO brands (id, name, website, description)
+VALUES (3, 'Lumma', 'https://website.com', 'Lumma is een merk cup');
+
+
+INSERT INTO pending_discs
+(created_date, username, id, brand, capacity, design_feature, is_reusable, model, name,
+ rim_width, width, material, shape, firmness, is_available_in_nl, link_to_store,
+ link_to_review, image)
+VALUES ('10-10-1988', 'Nynke', 1, 'Intimina', 20, 'geen steel', true, 'OS', 'Ziggy', 40, 40, 1, 'a', 'b', true, 'c',
+        'd', 'e');
+
