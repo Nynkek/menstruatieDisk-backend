ab743b924388c82f477eb166d7c98300f7871104
Nynke <35605149+Nynkek@users.noreply.github.com>
2022-07-01 11:01:07 +0200

    Update ExceptionController.java

--- src/main/java/nl/nynkek/menstruatiedisk/controllers/ExceptionController.java
+++ src/main/java/nl/nynkek/menstruatiedisk/controllers/ExceptionController.java
@@ -4,8 +4,10 @@ import nl.nynkek.menstruatiedisk.exeptions.RecordNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@CrossOrigin
 @ControllerAdvice
 public class ExceptionController {
 
