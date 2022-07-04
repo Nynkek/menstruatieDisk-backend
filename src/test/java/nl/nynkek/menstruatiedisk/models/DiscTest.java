b41b37631172f991e071be8fa7ac0bcf4a1ea0d8
Nynke <35605149+Nynkek@users.noreply.github.com>
2022-07-04 13:39:42 +0200

    eerste test

--- src/test/java/nl/nynkek/menstruatiedisk/models/DiscTest.java
+++ src/test/java/nl/nynkek/menstruatiedisk/models/DiscTest.java
@@ -1,9 +1,19 @@
 package nl.nynkek.menstruatiedisk.models;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiscTest {
 
-    
+    @Test
+    void test_ConstructorDisc() {
+
+        Disc disc = new Disc();
+
+        disc.setBrand("Ziggy");
+
+        assertEquals("Ziggy", disc.getBrand());
+    }
 
 }
